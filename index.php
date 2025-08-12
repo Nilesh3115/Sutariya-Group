476d1258a5baad6cdc2bc7418761c458c58f5922
Nilesh3115 <rn9319672@gmail.com>
2025-08-12 17:48:31 +0530

    Services Section Added

--- index.php
+++ index.php
@@ -15,6 +15,39 @@
     </div>
 </section>
 
+<!-- Services Section -->
+<section class="services section-mw section-p1 section-m1">
+    <h3>SERVICES</h3>
+    <h2>Transform your business with advanced technologies</h2>
+    <!-- Services Cards -->
+    <div class="services-grids">
+        <div class="service-grid s-1">
+        <h4>AI and Data & Analytics</h4>
+    </div>
+    <div class="service-grid s-2">
+        <h4>Education</h4>
+    </div>
+    <div class="service-grid s-3">
+        <h4>Travel & Logistics</h4>
+    </div>
+    <div class="service-grid s-4">
+        <h4>Network Solution & Services</h4>
+    </div>
+    <div class="service-grid s-5">
+        <h4>High Tech</h4>
+    </div>
+    <div class="service-grid s-6">
+        <h4>Manufacturing</h4>
+    </div>
+    <div class="service-grid s-7">
+        <h4>Retail</h4>
+    </div>
+    <div class="service-grid s-8">
+        <h4>Consulting</h4>
+    </div>
+    </div>
+</section>
+
 
 <!-- Footer Call -->
 <?php include 'footer.php'; ?>
